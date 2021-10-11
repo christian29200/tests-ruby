6cd9a9c3a49f309bc3a8951238482bfb44e76b19
christian29200 <christianmbarga29@yahoo.fr>
2021-10-11 23:53:22 +0200

    Initial 2

--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -1,15 +1,33 @@
-def who_is_bigger
+def who_is_bigger(a,b,c)
+    d = [a, b, c]
 
+    if a==nil || b==nil || c==nil
+        return "nil detected"
+
+    else
+        e = d.max
+        if d.index(e) == 0
+            return "a is bigger"
+        else
+            if d.index(e) == 1
+                return "b is bigger"
+            else
+                return "c is bigger"
+            end
+        end
+    end
 end
 
-def reverse_upcase_noLTA
-
+def reverse_upcase_noLTA(a)
+    return a.upcase.reverse!.delete! 'LTA'
 end
 
-def array_42
-
+def array_42(a)
+    return a.include?(42)
 end
 
-def magic_array
-
+def magic_array(a)
+    b = []
+    a.flatten.sort.uniq.each {|v| b << v * 2 if v % 3 != 0 }
+    return b
 end
